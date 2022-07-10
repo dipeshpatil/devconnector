80e48c395e1fc96bb453e0ca66868454b232b7e6
DipeshPTemp <dipeshptemp@Dipesh-Patils-MacBook-Pro.local>
2022-07-10 13:51:51 +0530

    Log in user route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
 
 // Middlewares
 const auth = require("../../middleware/auth");
@@ -19,5 +23,60 @@ router.get("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+// @route   POST api/auth
+// @desc    Authenticate User and Get Token
+// @access  Public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(req.body);
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials!" }] });
+      }
+
+      const isMatched = await bcrypt.compare(password, user.password);
+      if (!isMatched) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials!" }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        config.get("jwtOptions"),
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
