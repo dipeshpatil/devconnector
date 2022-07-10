c72eeb1b5afde74cfacd302979dbad9848b2e1b3
DipeshPTemp <dipeshptemp@Dipesh-Patils-MacBook-Pro.local>
2022-07-10 16:47:31 +0530

    Delete Profile, User and Posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -149,4 +149,23 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+/**
+ * @route   DELETE api/profile
+ * @desc    Delete profile, user and posts
+ * @access  Private
+ */
+ router.delete("/", auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+    // Remove Profile
+    await Profile.findOneAndRemove({user: req.user.id})
+    // Remove User
+    await User.findOneAndRemove({_id: req.user.id})
+    res.json({message: "User Deleted Successfully!"});
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
