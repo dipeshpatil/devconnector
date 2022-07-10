c1066b7434cc96f3f8641af02a7e6b228475dd81
DipeshPTemp <dipeshptemp@Dipesh-Patils-MacBook-Pro.local>
2022-07-10 14:22:40 +0530

    Profile Schema & Current User Profile Route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Profile route"));
+// Middlewares
+const auth = require("../../middleware/auth");
+
+// Models
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+
+// @route   GET api/profile/me
+// @desc    Get current user's Profile
+// @access  Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+    if (!profile) {
+      return res.status(400).json({ message: "Profile Doesn't Exist" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
