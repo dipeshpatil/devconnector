fcd17d3f9a4ba09c6c092b623ff3a10381ed4e14
Dipesh Patil <dipeshpatil@Ds-MacBook-Pro.local>
2022-11-05 16:09:59 +0530

    Prod Config

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const connectDB = require("./config/db");
 
 const userRoute = require("./routes/api/users");
@@ -14,13 +15,20 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
-
 // Define Routes
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postsRoute);
 app.use("/api/profile", profileRoute);
+
+// Serve Static Assets in Prod
+if (process.env.NODE_ENV === 'production') {
+  // Set Static Folder
+  app.use(express.static('client/build'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
